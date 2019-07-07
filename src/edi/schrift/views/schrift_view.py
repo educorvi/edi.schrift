02a5c5cddc87f22dc4dc76529c8a0f393826e6b6
Lars Walther <lwalther@novareto.de>
2019-07-07 17:59:56 +0200

    Schrift-View fertiggestellt

--- src/edi/schrift/views/schrift_view.py
+++ src/edi/schrift/views/schrift_view.py
@@ -2,16 +2,34 @@
 
 from edi.schrift import _
 from Products.Five.browser import BrowserView
+from edi.schrift.content.schrift import schrifttyp
 
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class SchriftView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('schrift_view.pt')
 
-    def __call__(self):
-        self.msg = _(u'A small message')
-        return self.index()
+    def getSchriftHeader(self):
+        header = {}
+        header['typ'] = schrifttyp.getTerm(self.context.typ).title
+        header['stand'] = self.context.modified().strftime('%d.%m.%Y')
+        if self.context.stand:
+            header['stand'] = self.context.stand.strftime('%s.%m.%Y')
+        return header
+
+    def getSchriftContent(self):
+        fc = self.context.getFolderContents()
+        html = ''
+        for i in fc:
+            obj = i.getObject()
+            if obj.portal_type == "Textbaustein":
+                if obj.text:
+                    html += obj.text.output
+            if obj.portal_type == "Image":
+                if obj.image:
+                    imgurl = '%s/@@images/image' % obj.absolute_url()
+                    html += '<p><img width="%s" class="img-responive inline" src="%s" title="%s"></p>' % (u'100%', imgurl, obj.title)
+                if obj.description:
+                    html += '<p class="discreet">%s</p>' % obj.description
+        return html
